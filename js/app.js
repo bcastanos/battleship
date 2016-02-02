22e3a20c8f50229a8fce00437521387e8becb652
Bryan Castanos <bcastanos@gmail.com>
2016-02-02 11:31:40 -0800

    added variables

--- js/app.js
+++ js/app.js
@@ -2,21 +2,22 @@ var allSquares = document.getElementsByClassName('square')
 var battleship = allSquares[Math.floor(Math.random() * allSquares.length)]
 var submarine = allSquares[Math.floor(Math.random() * allSquares.length)]
 var destroyer = allSquares[Math.floor(Math.random() * allSquares.length)]
+  battleship.className += ' ship'
+  submarine.className += ' ship'
+  destroyer.className += ' ship'
+var allShips = document.getElementsByClassName('ship')
 
-battleship.className += ' ship'
-submarine.className += ' ship'
-destroyer.className += ' ship'
+var explosion = 'explosion.jpg'
+console.log(explosion)
 
 
-// function checkShip() {
-//
-// }
-// allSquares.addEventListener('click', function(){
-//   console.log('Square clicked!!')
-// })
+
+
+
 
 battleship.addEventListener('click', function(){
   console.log('Battleship hit!')
+  console.log(battleship.innerHTML = 'hello')
 })
 
 destroyer.addEventListener('click', function(){
@@ -27,12 +28,15 @@ submarine.addEventListener('click', function(){
   console.log('Submarine hit!')
 })
 
-for(var i=0; i< allSquares.length; i++) {
-       allSquares[i].addEventListener("click", bindClick(i));
- }
 
- function bindClick(i) {
-    return function(){
-             console.log("you clicked region number " + i)
-           }
- }
+// for(var i=0; i< allSquares.length; i++) {
+//       allSquares[i].className += ' no ship'
+//        allSquares[i].addEventListener("click", bindClick(i));
+//
+//  }
+//
+//  function bindClick(i) {
+//     return function(){
+//              console.log("you clicked region number " + i)
+//            }
+//  }
