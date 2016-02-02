30e6971f38b5ddd33d2d555a8392e1bcfce6ce17
Bryan Castanos <bcastanos@gmail.com>
2016-02-02 15:25:11 -0800

    explosions set on click

--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@ var allSquares = document.getElementsByClassName('square')
 var battleship = allSquares[Math.floor(Math.random() * allSquares.length)]
 var submarine = allSquares[Math.floor(Math.random() * allSquares.length)]
 var destroyer = allSquares[Math.floor(Math.random() * allSquares.length)]
+
   battleship.className += ' ship'
   submarine.className += ' ship'
   destroyer.className += ' ship'
+
 var allShips = document.getElementsByClassName('ship')
 
-var explosion = 'explosion.jpg'
-console.log(explosion)
-
+$(allSquares).click(function() {
+  if ($(this).hasClass('ship')) {
+    $(this).html('<img src="explosion.jpg" alt="boom" />')
+  } else {
+  $(this).fadeTo('slow', .025)}
+})
 
 
 
@@ -17,26 +22,10 @@ console.log(explosion)
 
 battleship.addEventListener('click', function(){
   console.log('Battleship hit!')
-  console.log(battleship.innerHTML = 'hello')
 })
-
 destroyer.addEventListener('click', function(){
   console.log('Destroyer hit!')
 })
-
 submarine.addEventListener('click', function(){
   console.log('Submarine hit!')
 })
-
-
-// for(var i=0; i< allSquares.length; i++) {
-//       allSquares[i].className += ' no ship'
-//        allSquares[i].addEventListener("click", bindClick(i));
-//
-//  }
-//
-//  function bindClick(i) {
-//     return function(){
-//              console.log("you clicked region number " + i)
-//            }
-//  }
