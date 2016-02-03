93e86f34f3077af7e995f69bed5158e2ce8cf4e7
Bryan Castanos <bcastanos@gmail.com>
2016-02-02 16:27:15 -0800

    turn logic and basic score functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+var game = {
+  player1: {
+    name: "Player 1",
+    score: 0
+  },
+  player2: {
+    name: "Player 2",
+    score: 0
+  }
+}
+
+var currentPlayer = game.player1
+
+function switchTurn(){
+  if(currentPlayer == game.player1){
+    currentPlayer = game.player2
+  } else {
+    currentPlayer = game.player1
+  }
+  return currentPlayer
+}
+
+function incrementScore(){
+  currentPlayer.score += 1
+}
+
 var allSquares = document.getElementsByClassName('square')
 var battleship = allSquares[Math.floor(Math.random() * allSquares.length)]
 var submarine = allSquares[Math.floor(Math.random() * allSquares.length)]
@@ -9,11 +35,17 @@ var destroyer = allSquares[Math.floor(Math.random() * allSquares.length)]
 
 var allShips = document.getElementsByClassName('ship')
 
+
+
+
 $(allSquares).click(function() {
   if ($(this).hasClass('ship')) {
     $(this).html('<img src="explosion.jpg" alt="boom" />')
+    incrementScore()
+    
   } else {
   $(this).fadeTo('slow', .025)}
+  switchTurn()
 })
 
 
