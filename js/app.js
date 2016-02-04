31b13bf8ba1270e78686c73187f83b6b76b31d15
Bryan Castanos <bcastanos@gmail.com>
2016-02-03 17:30:23 -0800

    long sub!

--- js/app.js
+++ js/app.js
@@ -2,16 +2,45 @@ var scoreOne = document.getElementById('player1');
 var scoreTwo = document.getElementById('player2');
 var allSquares = document.getElementsByClassName('square')
 var battleship = allSquares[Math.floor(Math.random() * allSquares.length)]
-var submarine = allSquares[Math.floor(Math.random() * allSquares.length)]
+// var submarine = allSquares[Math.floor(Math.random() * allSquares.length)]
+var submarine = allSquares[21]
+
+
+
 var destroyer = allSquares[Math.floor(Math.random() * allSquares.length)]
 var allShips = document.getElementsByClassName('ship')
   battleship.className += ' ship'
   submarine.className += ' ship'
   destroyer.className += ' ship'
 
+
+
+  var freshBoat = []
   var boat = []
   var boatChoose = [Math.floor(Math.random() * allSquares.length)]
   var boatNum = boatChoose[0]
+
+  makeBoat()
+  var longSubmarine = []
+   longSubmarine[0] = allSquares[boat[0]]
+   longSubmarine[1] = allSquares[boat[1]]
+   longSubmarine[2] = allSquares[boat[2]]
+   longSubmarine[3] = allSquares[boat[3]]
+
+   longSubmarine[0].className += ' ship'
+   longSubmarine[1].className += ' ship'
+   longSubmarine[2].className += ' ship'
+   longSubmarine[3].className += ' ship'
+
+  // var newBoat = allSquares[boat1]
+  // var boat1 = makeBoat()
+
+  //
+  // for(i = 0; i < 3 ; i ++) {
+  //   var other = boat;
+  //   var sub[i] = allSquares[boat[i]]
+  //   console.log(other)
+  // }
 
   // for (var i = 0; i < 4; i++) {
   //   addOne = boatNum + 1;
@@ -25,11 +54,9 @@ var allShips = document.getElementsByClassName('ship')
   // }
 
   function makeBoat() {
-    // if (boatNum <= 3 || 7 <= boatNum <= 10 || 14 <= boatNum <= 17 ||
-    //   21 <= boatNum <= 24 || 28 <= boatNum <= 31 ||
-    //   35 <= boatNum <= 38 || 42 <= boatNum <= 45) {
-      console.log(boatNum)
-      if (boatNum <= 3 || boatNum >= 33) {
+    if (boatNum <= 3 || 7 <= boatNum && boatNum <= 10 || 14 <= boatNum && boatNum <= 17 ||
+      21 <= boatNum && boatNum <= 24 || 28 <= boatNum && boatNum <= 31 ||
+      35 <= boatNum && boatNum <= 38 || 42 <= boatNum && boatNum <= 45) {
       addOne = boatNum + 1;
       addTwo = boatNum + 2;
       addThree = boatNum + 3;
@@ -37,11 +64,29 @@ var allShips = document.getElementsByClassName('ship')
       boat.push(addOne);
       boat.push(addTwo);
       boat.push(addThree);
+    }
+    else if (4 <= boatNum && boatNum <= 6 || 11 <= boatNum && boatNum <= 13 ||
+      18 <= boatNum && boatNum <= 20) {
+      add7 = boatNum + 7;
+      add14 = boatNum + 14;
+      add21 = boatNum + 21;
+      boat.push(boatNum);
+      boat.push(add7);
+      boat.push(add14);
+      boat.push(add21);
       console.log(boat);}
-      else {console.log('else')}
-  }
-
-  makeBoat()
+    else {
+      sub1 = boatNum - 1;
+      sub2 = boatNum - 2;
+      sub3 = boatNum - 3;
+      boat.push(boatNum);
+      boat.push(sub1);
+      boat.push(sub2);
+      boat.push(sub3);
+      console.log(boat);
+      }
+    return boat;
+    }
 
 
 
@@ -49,9 +94,9 @@ var allShips = document.getElementsByClassName('ship')
 
 // console.log([Math.floor(Math.random() * allSquares.length)])
 
-function allSqRand() {
-  console.log(Math.random() * allSquares.length)
-}
+// function allSqRand() {
+//   console.log(Math.random() * allSquares.length)
+// }
 
 var game = {
   player1: {name: "Player 1",score: 0},
