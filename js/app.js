404754548a947319abb62625ee1761241239db26
Bryan Castanos <bcastanos@gmail.com>
2016-02-04 16:33:35 -0800

    no overlapping ships!

--- js/app.js
+++ js/app.js
@@ -7,61 +7,52 @@ var introMessage = $('#intro')
 var winMessage = $('#message')
 winMessage.hide()
 
-var battleArray = []
-var battleChoose = [Math.floor(Math.random() * allSquares.length)]
-var battleNum = battleChoose[0]
-
 $(introMessage).click(function() {
   introMessage.hide(1000)})
 
-  makeBattle()
-  var longBattleship = []
-   longBattleship[0] = allSquares[battleArray[0]]
-   longBattleship[1] = allSquares[battleArray[1]]
-   longBattleship[2] = allSquares[battleArray[2]]
-   longBattleship[3] = allSquares[battleArray[3]]
+function randomRanged(min,max){return Math.floor(Math.random()*(max-min+1)+min)}
 
-   longBattleship[0].className += ' ship'
-   longBattleship[1].className += ' ship'
-   longBattleship[2].className += ' ship'
-   longBattleship[3].className += ' ship'
+  var limitedBattleArray = []
+  var limitedBattleChoose = [randomRanged(0,13)]
+  var limitedBattleNum = limitedBattleChoose[0]
 
-  function makeBattle() {
-    if (battleNum <= 3 || 7 <= battleNum && battleNum <= 10 || 14 <= battleNum && battleNum <= 17 ||
-      21 <= battleNum && battleNum <= 24 || 28 <= battleNum && battleNum <= 31 ||
-      35 <= battleNum && battleNum <= 38 || 42 <= battleNum && battleNum <= 45) {
-      addOne = battleNum + 1;
-      addTwo = battleNum + 2;
-      addThree = battleNum + 3;
-      battleArray.push(battleNum);
-      battleArray.push(addOne);
-      battleArray.push(addTwo);
-      battleArray.push(addThree);
-    }
-    else if (4 <= battleNum && battleNum <= 6 || 11 <= battleNum && battleNum <= 13 ||
-      18 <= battleNum && battleNum <= 20) {
-      add7 = battleNum + 7;
-      add14 = battleNum + 14;
-      add21 = battleNum + 21;
-      battleArray.push(battleNum);
-      battleArray.push(add7);
-      battleArray.push(add14);
-      battleArray.push(add21);
+  makeLimitedBattle()
+
+  function makeLimitedBattle() {
+    if (limitedBattleNum <= 3 || 7 <= limitedBattleNum && limitedBattleNum <= 10){
+      addOne = limitedBattleNum + 1;
+      addTwo = limitedBattleNum + 2;
+      addThree = limitedBattleNum + 3;
+      limitedBattleArray.push(limitedBattleNum);
+      limitedBattleArray.push(addOne);
+      limitedBattleArray.push(addTwo);
+      limitedBattleArray.push(addThree);
     }
     else {
-      sub1 = battleNum - 1;
-      sub2 = battleNum - 2;
-      sub3 = battleNum - 3;
-      battleArray.push(battleNum);
-      battleArray.push(sub1);
-      battleArray.push(sub2);
-      battleArray.push(sub3);
-      }
-    return battleArray;
+      sub1 = limitedBattleNum - 1;
+      sub2 = limitedBattleNum - 2;
+      sub3 = limitedBattleNum - 3;
+      limitedBattleArray.push(limitedBattleNum);
+      limitedBattleArray.push(sub1);
+      limitedBattleArray.push(sub2);
+      limitedBattleArray.push(sub3);
+    }
+    return limitedBattleArray;
     }
 
+    var longBattleship = []
+     longBattleship[0] = allSquares[limitedBattleArray[0]]
+     longBattleship[1] = allSquares[limitedBattleArray[1]]
+     longBattleship[2] = allSquares[limitedBattleArray[2]]
+     longBattleship[3] = allSquares[limitedBattleArray[3]]
+
+     longBattleship[0].className += ' ship'
+     longBattleship[1].className += ' ship'
+     longBattleship[2].className += ' ship'
+     longBattleship[3].className += ' ship'
+
     var subArray = []
-    var subChoose = [Math.floor(Math.random() * allSquares.length)]
+    var subChoose = [randomRanged(14,20)]
     var subNum = subChoose[0]
 
     makeSub()
@@ -75,38 +66,16 @@ $(introMessage).click(function() {
      longSubmarine[2].className += ' ship'
 
     function makeSub() {
-      if (subNum <= 3 || 7 <= subNum && subNum <= 10 || 14 <= subNum && subNum <= 17 ||
-        21 <= subNum && subNum <= 24 || 28 <= subNum && subNum <= 31 ||
-        35 <= subNum && subNum <= 38 || 42 <= subNum && subNum <= 45) {
-        addOne = subNum + 1;
-        addTwo = subNum + 2;
-        addThree = subNum + 3;
-        subArray.push(subNum);
-        subArray.push(addOne);
-        subArray.push(addTwo);
-      }
-      else if (4 <= subNum && subNum <= 6 || 11 <= subNum && subNum <= 13 ||
-        18 <= subNum && subNum <= 20) {
         add7 = subNum + 7;
         add14 = subNum + 14;
-        add21 = subNum + 21;
         subArray.push(subNum);
         subArray.push(add7);
         subArray.push(add14);
-      }
-      else {
-        sub1 = subNum - 1;
-        sub2 = subNum - 2;
-        sub3 = subNum - 3;
-        subArray.push(subNum);
-        subArray.push(sub1);
-        subArray.push(sub2);
-        }
-      return subArray;
+        return subArray;
       }
 
       var destroyerArray = []
-      var destroyerChoose = [Math.floor(Math.random() * allSquares.length)]
+      var destroyerChoose = [randomRanged(35,41)]
       var destroyerNum = destroyerChoose[0]
 
     makeDestroy()
@@ -120,24 +89,13 @@ $(introMessage).click(function() {
      longDestroyer[2].className += ' ship'
 
     function makeDestroy() {
-      if (destroyerNum <= 3 || 7 <= destroyerNum && destroyerNum <= 10 || 14 <= destroyerNum && destroyerNum <= 17 ||
-        21 <= destroyerNum && destroyerNum <= 24 || 28 <= destroyerNum && destroyerNum <= 31 ||
-        35 <= destroyerNum && destroyerNum <= 38 || 42 <= destroyerNum && destroyerNum <= 45) {
+      if (35 <= destroyerNum && destroyerNum <= 39) {
         addOne = destroyerNum + 1;
         addTwo = destroyerNum + 2;
         addThree = destroyerNum + 3;
         destroyerArray.push(destroyerNum);
         destroyerArray.push(addOne);
         destroyerArray.push(addTwo);
-      }
-      else if (4 <= destroyerNum && destroyerNum <= 6 || 11 <= destroyerNum && destroyerNum <= 13 ||
-        18 <= destroyerNum && destroyerNum <= 20) {
-        add7 = destroyerNum + 7;
-        add14 = destroyerNum + 14;
-        add21 = destroyerNum + 21;
-        destroyerArray.push(destroyerNum);
-        destroyerArray.push(add7);
-        destroyerArray.push(add14);
       }
       else {
         sub1 = destroyerNum - 1;
@@ -148,6 +106,39 @@ $(introMessage).click(function() {
         destroyerArray.push(sub2);
         }
       return destroyerArray;
+      }
+
+      var carrierArray = []
+      var carrierChoose = [randomRanged(42,48)]
+      var carrierNum = carrierChoose[0]
+
+    makeCarrier()
+    var longCarrier = []
+     longCarrier[0] = allSquares[carrierArray[0]]
+     longCarrier[1] = allSquares[carrierArray[1]]
+     longCarrier[2] = allSquares[carrierArray[2]]
+
+     longCarrier[0].className += ' ship'
+     longCarrier[1].className += ' ship'
+     longCarrier[2].className += ' ship'
+
+    function makeCarrier() {
+      if (42 <= carrierNum && carrierNum <= 45) {
+        addOne = carrierNum + 1;
+        addTwo = carrierNum + 2;
+        carrierArray.push(carrierNum);
+        carrierArray.push(addOne);
+        carrierArray.push(addTwo);
+      }
+      else {
+        sub1 = carrierNum - 1;
+        sub2 = carrierNum - 2;
+        sub3 = carrierNum - 3;
+        carrierArray.push(carrierNum);
+        carrierArray.push(sub1);
+        carrierArray.push(sub2);
+        }
+      return carrierArray;
       }
 
 // console.log([Math.floor(Math.random() * allSquares.length)])
@@ -192,7 +183,7 @@ function switchTurn(){
 // }
 
 function checkWinner() {
-  if ((game.player1.score + game.player2.score) > 9) {
+  if ((game.player1.score + game.player2.score) > 12) {
     if (game.player1.score > game.player2.score) {
       winMessage.text('Player 1 Wins!');
       winMessage.show(1000);
@@ -236,35 +227,144 @@ $(allSquares).click(function() {
 //   submarine.className += ' ship'
 //   destroyer.className += ' ship'
 
+// INITIAL SHIP ARRAYS
 
-// battleship.addEventListener('click', function(){
-//   console.log('Battleship hit!')
-// })
-// destroyer.addEventListener('click', function(){
-//   console.log('Destroyer hit!')
-// })
-// submarine.addEventListener('click', function(){
-//   console.log('Submarine hit!')
-// })
+// var battleArray = []
+// var battleChoose = [Math.floor(Math.random() * allSquares.length)]
+// var battleNum = battleChoose[0]
 
-
-  // var newBoat = allSquares[boat1]
-  // var boat1 = makeBoat()
-
+  // makeBattle()
+  // var longBattleship = []
+  //  longBattleship[0] = allSquares[battleArray[0]]
+  //  longBattleship[1] = allSquares[battleArray[1]]
+  //  longBattleship[2] = allSquares[battleArray[2]]
+  //  longBattleship[3] = allSquares[battleArray[3]]
   //
-  // for(i = 0; i < 3 ; i ++) {
-  //   var other = boat;
-  //   var sub[i] = allSquares[boat[i]]
-  //   console.log(other)
-  // }
+  //  longBattleship[0].className += ' ship'
+  //  longBattleship[1].className += ' ship'
+  //  longBattleship[2].className += ' ship'
+  //  longBattleship[3].className += ' ship'
+  //
+  // function makeBattle() {
+  //   if (battleNum <= 3 || 7 <= battleNum && battleNum <= 10 || 14 <= battleNum && battleNum <= 17 ||
+  //     21 <= battleNum && battleNum <= 24 || 28 <= battleNum && battleNum <= 31 ||
+  //     35 <= battleNum && battleNum <= 38 || 42 <= battleNum && battleNum <= 45) {
+  //     addOne = battleNum + 1;
+  //     addTwo = battleNum + 2;
+  //     addThree = battleNum + 3;
+  //     battleArray.push(battleNum);
+  //     battleArray.push(addOne);
+  //     battleArray.push(addTwo);
+  //     battleArray.push(addThree);
+  //   }
+  //   else if (4 <= battleNum && battleNum <= 6 || 11 <= battleNum && battleNum <= 13 ||
+  //     18 <= battleNum && battleNum <= 20) {
+  //     add7 = battleNum + 7;
+  //     add14 = battleNum + 14;
+  //     add21 = battleNum + 21;
+  //     battleArray.push(battleNum);
+  //     battleArray.push(add7);
+  //     battleArray.push(add14);
+  //     battleArray.push(add21);
+  //   }
+  //   else {
+  //     sub1 = battleNum - 1;
+  //     sub2 = battleNum - 2;
+  //     sub3 = battleNum - 3;
+  //     battleArray.push(battleNum);
+  //     battleArray.push(sub1);
+  //     battleArray.push(sub2);
+  //     battleArray.push(sub3);
+  //     }
+  //   return battleArray;
+  //   }
 
-  // for (var i = 0; i < 4; i++) {
-  //   addOne = battleNum + 1;
-  //   addTwo = battleNum + 2;
-  //   addThree = battleNum + 3;
-  //   boat.push(battleNum);
-  //   boat.push(addOne);
-  //   boat.push(addTwo);
-  //   boat.push(addThree);
-  //   console.log(boat);
-  // }
+  // var subArray = []
+  // var subChoose = [Math.floor(Math.random() * allSquares.length)]
+  // var subNum = subChoose[0]
+  //
+  // makeSub()
+  // var longSubmarine = []
+  //  longSubmarine[0] = allSquares[subArray[0]]
+  //  longSubmarine[1] = allSquares[subArray[1]]
+  //  longSubmarine[2] = allSquares[subArray[2]]
+  //
+  //  longSubmarine[0].className += ' ship'
+  //  longSubmarine[1].className += ' ship'
+  //  longSubmarine[2].className += ' ship'
+  //
+  // function makeSub() {
+  //   if (subNum <= 3 || 7 <= subNum && subNum <= 10 || 14 <= subNum && subNum <= 17 ||
+  //     21 <= subNum && subNum <= 24 || 28 <= subNum && subNum <= 31 ||
+  //     35 <= subNum && subNum <= 38 || 42 <= subNum && subNum <= 45) {
+  //     addOne = subNum + 1;
+  //     addTwo = subNum + 2;
+  //     addThree = subNum + 3;
+  //     subArray.push(subNum);
+  //     subArray.push(addOne);
+  //     subArray.push(addTwo);
+  //   }
+  //   else if (4 <= subNum && subNum <= 6 || 11 <= subNum && subNum <= 13 ||
+  //     18 <= subNum && subNum <= 20) {
+  //     add7 = subNum + 7;
+  //     add14 = subNum + 14;
+  //     add21 = subNum + 21;
+  //     subArray.push(subNum);
+  //     subArray.push(add7);
+  //     subArray.push(add14);
+  //   }
+  //   else {
+  //     sub1 = subNum - 1;
+  //     sub2 = subNum - 2;
+  //     sub3 = subNum - 3;
+  //     subArray.push(subNum);
+  //     subArray.push(sub1);
+  //     subArray.push(sub2);
+  //     }
+  //   return subArray;
+  //   }
+
+//   var destroyerArray = []
+//   var destroyerChoose = [Math.floor(Math.random() * allSquares.length)]
+//   var destroyerNum = destroyerChoose[0]
+//
+// makeDestroy()
+// var longDestroyer = []
+//  longDestroyer[0] = allSquares[destroyerArray[0]]
+//  longDestroyer[1] = allSquares[destroyerArray[1]]
+//  longDestroyer[2] = allSquares[destroyerArray[2]]
+//
+//  longDestroyer[0].className += ' ship'
+//  longDestroyer[1].className += ' ship'
+//  longDestroyer[2].className += ' ship'
+//
+// function makeDestroy() {
+//   if (destroyerNum <= 3 || 7 <= destroyerNum && destroyerNum <= 10 || 14 <= destroyerNum && destroyerNum <= 17 ||
+//     21 <= destroyerNum && destroyerNum <= 24 || 28 <= destroyerNum && destroyerNum <= 31 ||
+//     35 <= destroyerNum && destroyerNum <= 38 || 42 <= destroyerNum && destroyerNum <= 45) {
+//     addOne = destroyerNum + 1;
+//     addTwo = destroyerNum + 2;
+//     addThree = destroyerNum + 3;
+//     destroyerArray.push(destroyerNum);
+//     destroyerArray.push(addOne);
+//     destroyerArray.push(addTwo);
+//   }
+//   else if (4 <= destroyerNum && destroyerNum <= 6 || 11 <= destroyerNum && destroyerNum <= 13 ||
+//     18 <= destroyerNum && destroyerNum <= 20) {
+//     add7 = destroyerNum + 7;
+//     add14 = destroyerNum + 14;
+//     add21 = destroyerNum + 21;
+//     destroyerArray.push(destroyerNum);
+//     destroyerArray.push(add7);
+//     destroyerArray.push(add14);
+//   }
+//   else {
+//     sub1 = destroyerNum - 1;
+//     sub2 = destroyerNum - 2;
+//     sub3 = destroyerNum - 3;
+//     destroyerArray.push(destroyerNum);
+//     destroyerArray.push(sub1);
+//     destroyerArray.push(sub2);
+//     }
+//   return destroyerArray;
+//   }
